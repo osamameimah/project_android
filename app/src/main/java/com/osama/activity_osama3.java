e9c7e26ab2fb14885b25e7c2960afeb2768e3c6a
yahya el saftawi <yahyaelsaftawi98@gmail.com>
2020-12-03 21:09:44 +0200

    first commit

---
+++ app/src/main/java/com/osama/activity_osama3.java
@@ -0,0 +1,14 @@
+package com.osama;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_osama3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_osama3);
+    }
+}
